c76be77e185c5f0aca4973e5ddb9eecdf333aca8
ilowen <lowen_i_v@hotmail.com>
2021-04-05 19:47:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const { Server } = require('ws');
+const express = require('express')
+const http = require('http')
+const WebSocket = require('ws')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const INDEX = '/index.html';
 
-const server = express();
+const port = process.env.PORT || 8080
+const app = express()
+const httpServer = http.createServer(app)
 
-server.use(express.static(__dirname));
+app.use(express.static(__dirname));
 
-server.use("/", (req, res) => res.sendFile(INDEX, { root: __dirname }));
-
-server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.use("/", (req, res) => res.sendFile(INDEX, { root: __dirname }));
 
 
-const wss = new Server({ server });
 
+
+const wss = new WebSocket.Server({
+    'server': httpServer
+})
+httpServer.listen(port);
 
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
